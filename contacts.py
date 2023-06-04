963210cd47b9a0a2b0bb6867509714385ee0a32a
rezatn0934 <rezatn0934@gmail.com>
2023-06-05 01:27:37 +0330

    fixing add contact method bugs

--- contacts.py
+++ contacts.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass
 import pickle
+import os
 
 
 @dataclass
@@ -20,23 +21,28 @@ class Contact:
 
     @classmethod
     def add_contact(cls, name, email, phone):
-        contacts = [cls.load_from_pickle()]
         contact = Contact(name=name, email=email, phone=phone)
-        contacts.append(contact)
-        print(contacts)
+        if "contacts.pickle" in os.listdir():
+            if isinstance(cls.load_from_pickle(), list):
+                contacts = cls.load_from_pickle()
+                contacts.append(contact)
+            else:
+                contacts = [cls.load_from_pickle(), contact]
+        else:
+            contacts = contact
         cls.save_to_pickle(contacts)
 
     @classmethod
     def edite_contact(cls, name, email=None, phone=None):
         contacts = cls.load_from_pickle()
-        for contact in [contacts]:
+        for num, contact in enumerate(contacts):
             if contact.name == name:
                 if email:
                     contact.email = email
                 if phone:
                     contact.phone = phone
-                cls.save_to_pickle(contact)
+                contacts[num]=contact
+                cls.save_to_pickle(contacts)
                 break
-
 
 
