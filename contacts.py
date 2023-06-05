1d54c50e287b07f5e6bb5ab0ac5282731e319398
rezatn0934 <rezatn0934@gmail.com>
2023-06-05 19:46:28 +0330

    Create a method to check contacts and prevent the creation of duplicates

--- contacts.py
+++ contacts.py
@@ -24,8 +24,10 @@ class Contact:
         contact = Contact(name=name, email=email, phone=phone)
         if "contacts.pickle" in os.listdir():
             if isinstance(cls.load_from_pickle(), list):
+                result = cls.check_contact(contact)
                 contacts = cls.load_from_pickle()
-                contacts.append(contact)
+                if result:
+                    contacts.append(contact)
             else:
                 contacts = [cls.load_from_pickle(), contact]
         else:
@@ -45,4 +47,12 @@ class Contact:
                 cls.save_to_pickle(contacts)
                 break
 
+    @classmethod
+    def check_contact(cls, in_contact):
+        contacts = cls.load_from_pickle()
+        for i, contact in enumerate(contacts):
+            if contact.name == in_contact.name and contact.email == in_contact.email and contact.phone == in_contact.phone:
+                print("Contact already exist!!")
+                return False
+        return True
 
