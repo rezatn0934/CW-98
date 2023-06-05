b78df7bbf42884890bc1d6ed8d0a24dfa105e713
rezatn0934 <rezatn0934@gmail.com>
2023-06-05 21:11:48 +0330

    Define save to file method

--- users.py
+++ users.py
@@ -1,7 +1,12 @@
 from dataclasses import dataclass
+import pickle
 
 @dataclass
 class User:
     username: str
     password: str
 
+    @classmethod
+    def save_to_pickle(cls, obj):
+        with open("User.pickle", "wb") as f:
+            pickle.dump(obj, f)
