8d0b0672ade7a188a4b73fa7f8fc50d7f6097845
rezatn0934 <rezatn0934@gmail.com>
2023-06-05 21:17:48 +0330

    Define add user method

--- users.py
+++ users.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass
 import pickle
+import os
 
 @dataclass
 class User:
@@ -8,10 +9,25 @@ class User:
 
     @classmethod
     def save_to_pickle(cls, obj):
-        with open("User.pickle", "wb") as f:
+        with open("users.pickle", "wb") as f:
             pickle.dump(obj, f)
 
     @classmethod
     def load_from_pickle(cls):
-        with open("User.pickle", "rb") as f:
+        with open("users.pickle", "rb") as f:
             return pickle.load(f)
+
+    @classmethod
+    def add_user(cls, name, password):
+        user = User(username=name, password=password)
+        if "users.pickle" in os.listdir():
+            if isinstance(cls.load_from_pickle(), list):
+                result = cls.check_user(user)
+                users = cls.load_from_pickle()
+                if result:
+                    users.append(user)
+            else:
+                users = [cls.load_from_pickle(), user]
+        else:
+            users = user
+        cls.save_to_pickle(users)
