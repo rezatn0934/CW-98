6ba105363467f5afd98f977a64c162c32c225154
rezatn0934 <rezatn0934@gmail.com>
2023-06-15 15:31:28 +0330

    Working with sys.argv inputs

--- CW/11/1.py
+++ CW/11/1.py
@@ -8,3 +8,16 @@ def read_file(file_name, mod):
     return content
 
 
+if len(sys.argv) == 1:
+    print("Hello word")
+elif os.path.exists(sys.argv[1]):
+    string = read_file(sys.argv[1], mod= "r")
+    lst1 = string.split()
+    dic1 = dict()
+    for word in lst1:
+        dic1[word] = dic1.get(word, 0) + 1
+    print(dic1)
+else:
+    print(sys.argv[1][::-1])
+
+
