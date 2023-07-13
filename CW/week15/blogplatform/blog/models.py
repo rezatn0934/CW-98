9b3148cd15aad602ee61d9679c5572e1524aaa7a
rezatn0934 <rezatn0934@gmail.com>
2023-07-13 16:33:40 +0330

    feat: creat model classes

--- CW/week15/blogplatform/blog/models.py
+++ CW/week15/blogplatform/blog/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=50)
+    content = models.TextField()
+    author = models.CharField(max_length=50)
+    publication_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Category(models.Model):
+     name = models.CharField(max_length=50)
+     description = models.TextField()
+
+
+class Comment(models.Model):
+    post = models.CharField(max_length=50)
+    author = models.CharField(max_length=50)
