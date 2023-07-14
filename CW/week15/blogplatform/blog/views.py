e926279f7018cf88dd551c9455c4dfdf26fd54db
rezatn0934 <rezatn0934@gmail.com>
2023-07-14 09:15:35 +0330

    feat: define view_category func

--- CW/week15/blogplatform/blog/views.py
+++ CW/week15/blogplatform/blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Post
+from .models import Post, Category
 # Create your views here.
 
 
@@ -11,6 +11,12 @@ def view_post(request):
     posts = Post.objects.all()
     return render(request, 'blog/post.html', {'posts': posts})
 
+
 def view_post_detail(request, pk):
     post = Post.objects.get(pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
+
+
+def view_category(request):
+    categorys = Category.objects.all()
+    return render(request, 'blog/category.html', {'categorys': categorys})
