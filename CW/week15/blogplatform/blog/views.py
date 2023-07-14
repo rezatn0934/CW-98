369433310b19c73886fdf37f51c0da90efdcba37
rezatn0934 <rezatn0934@gmail.com>
2023-07-14 09:13:19 +0330

    feat: define view_post func

--- CW/week15/blogplatform/blog/views.py
+++ CW/week15/blogplatform/blog/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
-
+from .models import Post
 # Create your views here.
 
 
 def view_home(request):
     return render(request, 'blog/home.html')
+
+
+def view_post(request):
+    posts = Post.objects.all()
+    return render(request, 'blog/post.html', {'posts': posts})
