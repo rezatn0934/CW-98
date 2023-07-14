ad76c9c12024e8f137deb3a4d1c440ed369bba8f
rezatn0934 <rezatn0934@gmail.com>
2023-07-14 11:09:42 +0330

    feat: define relation between models

--- CW/week15/blogplatform/blog/models.py
+++ CW/week15/blogplatform/blog/models.py
@@ -6,8 +6,9 @@ from django.db import models
 class Post(models.Model):
     title = models.CharField(max_length=50)
     content = models.TextField()
-    author = models.CharField(max_length=50)
+    author = models.ForeignKey('Author', on_delete=models.CASCADE)
     publication_date = models.DateTimeField(auto_now_add=True)
+    category = models.ForeignKey('Category', on_delete=models.PROTECT)
 
     def __str__(self):
         return self.title
@@ -20,9 +21,10 @@ class Category(models.Model):
      def __str__(self):
          return self.name
 
+
 class Comment(models.Model):
-    post = models.CharField(max_length=50)
-    author = models.CharField(max_length=50)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    author = models.ForeignKey('Author', on_delete=models.CASCADE)
     content = models.TextField()
 
     def __str__(self):
