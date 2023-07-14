63841aa40716be6fa482a2b6afcce48061e22595
rezatn0934 <rezatn0934@gmail.com>
2023-07-14 09:18:49 +0330

    feat: add posts path

--- CW/week15/blogplatform/blog/urls.py
+++ CW/week15/blogplatform/blog/urls.py
@@ -2,9 +2,11 @@
 
 from django.contrib import admin
 from django.urls import path
-from .views import view_home
+from .views import view_home, view_post, view_post_detail
 
 
 urlpatterns = [
     path('', view_home),
+    path('post/', view_post),
+    path('post/<int:pk>/', view_post_detail, name='post_detail'),
 ]
