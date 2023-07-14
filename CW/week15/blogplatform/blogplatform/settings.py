2f84c34015346966ee6489eec00744f52ca32721
rezatn0934 <rezatn0934@gmail.com>
2023-07-14 10:45:52 +0330

    feat: add STATIC_ROOT

--- CW/week15/blogplatform/blogplatform/settings.py
+++ CW/week15/blogplatform/blogplatform/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'blogplatform.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [str(BASE_DIR.joinpath('template'))],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,7 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT = 'static'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
