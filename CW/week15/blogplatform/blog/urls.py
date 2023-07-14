9d419e70988a2e8b2f7be2439b4ed8538d30562a
rezatn0934 <rezatn0934@gmail.com>
2023-07-14 09:22:29 +0330

    feat: add category path

--- CW/week15/blogplatform/blog/urls.py
+++ CW/week15/blogplatform/blog/urls.py
@@ -2,11 +2,13 @@
 
 from django.contrib import admin
 from django.urls import path
-from .views import view_home, view_post, view_post_detail
+from .views import view_home, view_post, view_post_detail, view_category, view_category_detail
 
 
 urlpatterns = [
     path('', view_home),
-    path('post/', view_post),
+    path('post/', view_post, name='post'),
     path('post/<int:pk>/', view_post_detail, name='post_detail'),
+    path('category/', view_category, name='category'),
+    path('category/<int:pk>/', view_category_detail, name='category_detail'),
 ]
