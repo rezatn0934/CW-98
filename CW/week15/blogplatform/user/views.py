4c0aa651384dcd04171eb0e1aab3fabc6823411b
rezatn0934 <rezatn0934@gmail.com>
2023-07-14 09:33:31 +0330

    feat: define author view func

--- CW/week15/blogplatform/user/views.py
+++ CW/week15/blogplatform/user/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from .models import Author
 # Create your views here.
+
+def view_author(request):
+    authors = Author.objects.all()
+    return render(request, 'blog/author.html', {'poauthorssts': authors})
