303fbf22bfc7f36f9893727321543184be94824d
rezatn0934 <rezatn0934@gmail.com>
2023-07-14 10:42:58 +0330

    refactor: refactor views name

--- CW/week15/blogplatform/blog/urls.py
+++ CW/week15/blogplatform/blog/urls.py
@@ -2,13 +2,13 @@
 
 from django.contrib import admin
 from django.urls import path
-from .views import view_home, view_post, view_post_detail, view_category, view_category_detail
+from .views import view_home, view_posts, view_post_detail, view_categorys, view_category_detail
 
 
 urlpatterns = [
     path('', view_home),
-    path('post/', view_post, name='post'),
+    path('posts/', view_posts, name='posts'),
     path('post/<int:pk>/', view_post_detail, name='post_detail'),
-    path('category/', view_category, name='category'),
+    path('categorys/', view_categorys, name='categorys'),
     path('category/<int:pk>/', view_category_detail, name='category_detail'),
 ]
