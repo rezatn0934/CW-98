d51992b929c334dec0c9f46cbd8079bd7b7e4168
rezatn0934 <rezatn0934@gmail.com>
2023-07-14 12:16:56 +0330

    chore: modify passing models to ForeignKey method

--- CW/week15/blogplatform/blog/models.py
+++ CW/week15/blogplatform/blog/models.py
@@ -1,30 +1,30 @@
 from django.db import models
-
+from user.models import Author
 # Create your models here.
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
 
 
 class Post(models.Model):
     title = models.CharField(max_length=50)
     content = models.TextField()
-    author = models.ForeignKey('Author', on_delete=models.CASCADE)
+    author = models.ForeignKey(Author, on_delete=models.PROTECT)
     publication_date = models.DateTimeField(auto_now_add=True)
-    category = models.ForeignKey('Category', on_delete=models.PROTECT)
+    category = models.ForeignKey(Category, on_delete=models.PROTECT)
 
     def __str__(self):
         return self.title
 
 
-class Category(models.Model):
-     name = models.CharField(max_length=50)
-     description = models.TextField()
-
-     def __str__(self):
-         return self.name
-
-
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
-    author = models.ForeignKey('Author', on_delete=models.CASCADE)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
     content = models.TextField()
 
     def __str__(self):
