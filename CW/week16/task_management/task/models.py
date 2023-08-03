4f5649d91925f3b5087c17a608b1728e5d7576b3
rezatn0934 <rezatn0934@gmail.com>
2023-08-03 16:33:32 +0330

    chore: create relation between task and user

--- CW/week16/task_management/task/models.py
+++ CW/week16/task_management/task/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from user.models import CustomUser
 
 # Create your models here.
 class Category(models.Model):
@@ -36,6 +36,7 @@ class Task(models.Model):
         max_length=1, choices=STATUS_CHOICES, default=STATUS_UNASSIGNED)
     category = models.ForeignKey(Category, on_delete=models.PROTECT)
     tag = models.ManyToManyField(Tag)
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
