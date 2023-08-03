42c3c7b9d4e9d3eb23534af04d09f8d7240a59b6
rezatn0934 <rezatn0934@gmail.com>
2023-08-03 16:57:02 +0330

    chore: define create user form

--- CW/week16/task_management/user/admin.py
+++ CW/week16/task_management/user/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from django import forms
+from .models import CustomUser
+
 
 # Register your models here.
+
+
+class CreateUser(forms.ModelForm):
+    class Meta:
+        model = CustomUser
+        fields = ('username', 'password')
