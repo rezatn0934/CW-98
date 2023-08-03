90c19adc144eb991d7883ac7bd9406adcc80bcc1
rezatn0934 <rezatn0934@gmail.com>
2023-08-03 16:20:32 +0330

    chore: update gitgnor

--- CW/week16/task_management/task_management/settings.py
+++ CW/week16/task_management/task_management/settings.py
@@ -130,3 +130,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+AUTH_USER_MODEL = "user.CustomUser"
