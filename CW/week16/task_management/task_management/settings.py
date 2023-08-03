0dbacbbd5f20433b86832b78d3e509d9d12fed21
rezatn0934 <rezatn0934@gmail.com>
2023-08-03 16:04:29 +0330

    chore: add user app to installed_app

--- CW/week16/task_management/task_management/settings.py
+++ CW/week16/task_management/task_management/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'task',
+    'user',
 ]
 
 MIDDLEWARE = [
@@ -48,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
 ]
 
 ROOT_URLCONF = 'task_management.urls'
