e433d7021b7eb959a53edc378fb8a7147ac898a5
rezatn0934 <rezatn0934@gmail.com>
2023-08-04 21:33:01 +0330

    refac: refactor login form to use custom authentication

--- CW/week16/task_management/user/views.py
+++ CW/week16/task_management/user/views.py
@@ -1,35 +1,23 @@
 from django.shortcuts import render, redirect
-from .forms import LoginForm
-from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.hashers import make_password
+from .forms import LoginForm, UserForm
 from .models import CustomUser
-
-
-# Create your views here.
+from .authentication import AuthBackend
 
 
 def user_login(request):
     message = None
     if request.method == "POST":
-        print('20' * 100)
         form = LoginForm(request.POST)
-        print(form.is_valid())
         if form.is_valid():
-            user = form.save()
-            print(type(user))
-            print(user.username)
-            # user = authenticate(request, username=cd["username"], password=cd["password"])
-            if user is not None:
-                print('1' * 100)
-                login(request, user)
-                return render(request, 'user/dashboard.html', {'user': user})
-            else:
-                message = "Invalid phone number or password"
+            cd = form.cleaned_data
+            user = cd['user']
+            login(request, user, backend='user.authentication.AuthBackend')
+            return redirect('/')
+        message = "Invalid login credentials"
     elif request.method == "GET":
-        if request.user.is_authenticated:
-            return redirect('dashboard')
-    form = LoginForm()
+        form = LoginForm()
     return render(request, "user/login.html", {"message": message, "form": form})
 
 
-def dashboard(request):
-    return render(request, "user/dashboard.html")
