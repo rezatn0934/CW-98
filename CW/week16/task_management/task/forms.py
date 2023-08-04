17e063271e9b948a4d7ec7f6d3c0cd5605167739
rezatn0934 <rezatn0934@gmail.com>
2023-08-04 22:59:40 +0330

    chore: define UpdateCategoryForm

--- CW/week16/task_management/task/forms.py
+++ CW/week16/task_management/task/forms.py
@@ -6,3 +6,15 @@ class CreatTaskForm(forms.ModelForm):
     class Meta:
         model = Task
         fields = '__all__'
+
+
+class UpdateCategoryForm(forms.ModelForm):
+    class Meta:
+        model = Category
+        fields = ['name', 'description']
+        widgets = {
+            'name': forms.TextInput(
+                attrs={'class': 'form-control', 'placeholder': 'Name', 'style': 'text-align: center;'}),
+            'description': forms.Textarea(
+                attrs={'class': 'form-control', 'placeholder': 'Description', 'style': 'text-align: center;'}),
+        }
