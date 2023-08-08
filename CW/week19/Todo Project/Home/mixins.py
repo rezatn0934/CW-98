4fbac548fe6585c83497befe753bb2e46702d388
rezatn0934 <rezatn0934@gmail.com>
2023-08-08 19:08:29 +0330

    fix: change id to pk

--- CW/week19/Todo Project/Home/mixins.py
+++ CW/week19/Todo Project/Home/mixins.py
@@ -8,16 +8,16 @@ class TodoMixin:
     template_name = None
 
     def dispatch(self, request, *args, **kwargs):
-        todo = Todo.objects.get(id=kwargs['id'])
+        todo = Todo.objects.get(id=kwargs['pk'])
         if not todo.user == request.user:
             raise PermissionDenied
 
-    def get(self, request, id):
-        todo = Todo.objects.filter(id=id)
+    def get(self, request, pk):
+        todo = Todo.objects.filter(id=pk)
         return render(request, self.template_name, {'todo': todo})
 
-    def post(self, request, id):
-        todo = Todo.objects.get(id=id)
+    def post(self, request, pk):
+        todo = Todo.objects.get(id=pk)
         form = self.form_class(request.POST, instance=todo)
         if form.is_valid():
             todo.save()
