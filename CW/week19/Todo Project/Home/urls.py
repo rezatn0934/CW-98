d3f98aae8a1bb841b8cd61c9ba229d32c3a444db
rezatn0934 <rezatn0934@gmail.com>
2023-08-08 19:08:55 +0330

    chore: add admin path to urls

--- CW/week19/Todo Project/Home/urls.py
+++ CW/week19/Todo Project/Home/urls.py
@@ -1,7 +1,9 @@
+from django.contrib import admin
 from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('', views.IndexView.as_view(), name='index'),
     path('todos/', views.TodoListView.as_view(), name='todo_list'),
     path('todos/<int:pk>/', views.TodoDetailView.as_view(), name='todo_detail'),
