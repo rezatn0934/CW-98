b0444d4e312c31961695980a948c2e519259b900
rezatn0934 <rezatn0934@gmail.com>
2023-08-08 18:48:52 +0330

    chore: add max_length to title

--- CW/week19/Todo Project/Home/models.py
+++ CW/week19/Todo Project/Home/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 
 class Todo(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    title = models.CharField()
+    title = models.CharField(max_length=50)
     description = models.TextField(null=True, blank=True)
     is_completed = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
