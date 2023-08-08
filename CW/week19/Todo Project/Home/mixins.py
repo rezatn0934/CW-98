2fc756aaf2b7012b7bdcf4dec3b630197aaec8af
rezatn0934 <rezatn0934@gmail.com>
2023-08-08 20:12:42 +0330

    chore: define todo form

--- CW/week19/Todo Project/Home/mixins.py
+++ CW/week19/Todo Project/Home/mixins.py
@@ -10,7 +10,7 @@ class TodoMixin:
     def dispatch(self, request, *args, **kwargs):
         todo = Todo.objects.get(id=kwargs['pk'])
         if not todo.user == request.user:
-            raise PermissionDenied
+            return redirect('permision')
         return super().dispatch(request, *args, **kwargs)
 
     def get(self, request, pk):
@@ -20,9 +20,11 @@ class TodoMixin:
         return render(request, self.template_name, {'todo': todo, 'form': form})
 
     def post(self, request, pk):
-        todo = Todo.objects.get(id=pk)
-        form = self.form_class(request.POST, instance=todo)
+        todo1 = Todo.objects.get(id=pk)
+        form = self.form_class(request.POST)
         if form.is_valid():
+            todo = form.save(commit=False)
+            todo.user = todo1.user
             todo.save()
             return redirect('thank_you', todo.id)
         return render(request, self.template_name, {'todo': todo})
