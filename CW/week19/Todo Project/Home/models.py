f1b2c6fb4874ecbfcede7249458d220336ffa48f
rezatn0934 <rezatn0934@gmail.com>
2023-08-08 18:52:31 +0330

    fix: remove meta class

--- CW/week19/Todo Project/Home/models.py
+++ CW/week19/Todo Project/Home/models.py
@@ -10,8 +10,6 @@ class Todo(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
-    class Meta:
-        abstract = True
 
     def __str__(self):
         return self.title
