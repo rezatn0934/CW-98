49fb97f4b7428321ef3d15ca92d74aa272fe772a
rezatn0934 <rezatn0934@gmail.com>
2023-08-08 18:43:56 +0330

    fix: add parent for as_views

--- CW/week19/Todo Project/Home/urls.py
+++ CW/week19/Todo Project/Home/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.IndexView.as_view, name='index'),
-    path('todos/', views.TodoListView.as_view, name='todo_list'),
-    path('todos/<int:pk>/', views.TodoDetailView.as_view, name='todo_detail'),
+    path('', views.IndexView.as_view(), name='index'),
+    path('todos/', views.TodoListView.as_view(), name='todo_list'),
+    path('todos/<int:pk>/', views.TodoDetailView.as_view(), name='todo_detail'),
 ]
