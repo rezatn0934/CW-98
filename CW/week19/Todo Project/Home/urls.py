1102cb2fdb90fdd2de7656eb8ca72362bfaf944c
rezatn0934 <rezatn0934@gmail.com>
2023-08-08 19:52:46 +0330

    chore: add TankYou url pattern

--- CW/week19/Todo Project/Home/urls.py
+++ CW/week19/Todo Project/Home/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.IndexView.as_view(), name='index'),
     path('todos/', views.TodoListView.as_view(), name='todo_list'),
-    path('todos/<int:pk>/', views.TodoDetailView.as_view(), name='todo_detail'),
+    path('todos/<int:pk>', views.TodoDetailView.as_view(), name='todo_detail'),
+    path('todos/<int:pk>', views.TankYou.as_view(), name='thank_you')
 ]
