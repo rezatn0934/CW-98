3bf89b6c916d5c8e6cbfa39fcd5b678da343e152
rezatn0934 <rezatn0934@gmail.com>
2023-08-11 11:58:37 +0330

    refac: define img_preview

--- CW/week16/task_management/user/models.py
+++ CW/week16/task_management/user/models.py
@@ -1,10 +1,17 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.utils.html import mark_safe
+
+
 # Create your models here.
 
 
 class CustomUser(AbstractUser):
     image = models.ImageField(upload_to='images/product/', blank=True, null=True)
+
+    def img_preview(self):
+        if self.image:
+            return mark_safe(f'<img src = "{self.image.url}" width = "150" height="150"/>')
 
     def __str__(self):
         return self.username
