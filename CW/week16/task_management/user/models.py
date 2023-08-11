31de99d34a05ab628d55e7d39eb0fac6db325f01
rezatn0934 <rezatn0934@gmail.com>
2023-08-11 12:01:18 +0330

    refac: define overwrite save method to manage media files

--- CW/week16/task_management/user/models.py
+++ CW/week16/task_management/user/models.py
@@ -1,17 +1,23 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.utils.html import mark_safe
-
+from .mixins import ImageMixin
 
 # Create your models here.
 
 
-class CustomUser(AbstractUser):
+class CustomUser(ImageMixin, AbstractUser):
     image = models.ImageField(upload_to='images/product/', blank=True, null=True)
 
     def img_preview(self):
         if self.image:
             return mark_safe(f'<img src = "{self.image.url}" width = "150" height="150"/>')
+
+    def save(self, *args, **kwargs):
+        if self.pk:
+            old_instance = CustomUser.objects.get(pk=self.pk)
+            self.change_image(old_instance, "image")
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.username
