68bc5e64746726450e78c921864aa2114b3e81f9
rezatn0934 <rezatn0934@gmail.com>
2023-08-11 11:19:53 +0330

    refac: add change pass view to urls

--- CW/week16/task_management/user/urls.py
+++ CW/week16/task_management/user/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('login/', views.user_login, name='login'),
     path('profile/', views.ProfileView.as_view(), name='profile'),
     path('register/', views.register_user, name='register'),
+    path('change_pass/', views.ChangePassView.as_view(), name='change_pass'),
     path('logout/', views.log_out, name='logout')
 ]
