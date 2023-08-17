452219f69d1afef50874207b991039ee043c2dc2
rezatn0934 <rezatn0934@gmail.com>
2023-08-17 17:37:51 +0330

    chore: define login view urls

--- CW/week16/task_management/user/urls.py
+++ CW/week16/task_management/user/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path, include
+from django.contrib.auth.views import LogoutView
 from . import views
 
 urlpatterns = [
-    path('login/', views.user_login, name='login'),
+    path('login/', views.UserLogin.as_view(), name='login'),
     path('profile/', views.ProfileView.as_view(), name='profile'),
     path('register/', views.register_user, name='register'),
     path('change_pass/', views.ChangePassView.as_view(), name='change_pass'),
