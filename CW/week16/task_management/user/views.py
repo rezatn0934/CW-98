a7997b52d6f69c620c6b07b8d5bd780098725c37
rezatn0934 <rezatn0934@gmail.com>
2023-08-17 17:37:03 +0330

    refac: change login view to use LoginView generic class

--- CW/week16/task_management/user/views.py
+++ CW/week16/task_management/user/views.py
@@ -1,28 +1,25 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
-from django.contrib.auth import login, logout
+from django.urls import reverse_lazy
+from django.contrib.auth import login
+from django.contrib.auth.views import LoginView
 from django.contrib.auth.hashers import make_password
 from django.views import View
-
-from .forms import LoginForm, UserForm, ChangePassForm
+from .forms import UserForm, ChangePassForm
 from .mixins import ProfileMixin
-from .models import CustomUser
-from .authentication import AuthBackend
 
 
-def user_login(request):
-    message = None
-    if request.method == "POST":
-        form = LoginForm(request.POST)
-        if form.is_valid():
-            cd = form.cleaned_data
-            user = cd['user']
-            login(request, user, backend='user.authentication.AuthBackend')
-            return redirect('/')
-        message = "Invalid login credentials"
-    elif request.method == "GET":
-        form = LoginForm()
-    return render(request, "user/login.html", {"message": message, "form": form})
+class UserLogin(LoginView):
+    redirect_authenticated_user = True
+    template_name = 'user/login.html'
+
+    def get_success_url(self):
+        return reverse_lazy('home_page')
+
+    def form_valid(self, form):
+        user = form.get_user()
+        login(self.request, user, backend='user.authentication.AuthBackend')
+        return super().form_invalid(form)
 
 
 def register_user(request):
