3e068c41a3c23b7b78af21e370c9597fe6c9ddf4
rezatn0934 <rezatn0934@gmail.com>
2023-08-17 15:10:43 +0330

    refac: change home view urls to us class base Listview

--- CW/week16/task_management/task/urls.py
+++ CW/week16/task_management/task/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home_view, name='home_page'),
+    path('', views.HomeView.as_view(), name='home_page'),
     path('tasks/', views.all_seeing_eye_view, name="task_list"),
     path('task_detail/<int:pk>/', views.TaskDetail.as_view(), name='task_detail'),
     path('search/', views.task_search_view, name='search'),
