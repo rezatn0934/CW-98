59082534ad260ca09d934ffa7a1e1d991bd1d146
rezatn0934 <rezatn0934@gmail.com>
2023-08-18 11:35:30 +0330

    chore: add export to csv

--- CW/week16/task_management/task/admin.py
+++ CW/week16/task_management/task/admin.py
@@ -1,14 +1,32 @@
 from django.contrib import admin
+from django.contrib import messages
 from .models import Task, Category, Tag
 # Register your models here.
+
+import csv
 
 
 @admin.register(Task)
 class TaskAdmin(admin.ModelAdmin):
+    actions = ['export_to_csv']
     list_display = ('title', 'due_date', 'user', 'status', 'category', 'created', 'updated', 'description')
     list_editable = ('status',)
     list_filter = ('status', 'category')
     search_fields = ('title', 'category__name')
+
+    @admin.action(description='Export To csv')
+    def export_to_csv(self, request, queryset):
+        header = ['title', 'due_date', 'status', 'category', 'tag', 'user', 'created',
+                  'updated', 'is_active']
+        with open('task/task.csv', 'w', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerow(header)
+            for item in queryset:
+                writer.writerow(
+                    [item.title, item.due_date, item.status, item.category, item.tag, item.user, item.created,
+                     item.updated, item.is_active])
+
+        messages.success(request, 'CSV file for selected model has been successfully created at task/task.csv')
 
 
 @admin.register(Category)
