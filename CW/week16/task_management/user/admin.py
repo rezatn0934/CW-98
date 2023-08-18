4e740c7e1425630f06cc15d27b4204e2dfd3e2f8
rezatn0934 <rezatn0934@gmail.com>
2023-08-18 10:18:35 +0330

    chore: define query_set function to count number of each user task

--- CW/week16/task_management/user/admin.py
+++ CW/week16/task_management/user/admin.py
@@ -1,8 +1,21 @@
 from django.contrib import admin
+from django.db.models import Count
+from django.urls import reverse
+from django.utils.html import format_html, urlencode
+
 from django import forms
 from .models import CustomUser
 
 
 # Register your models here.
 
-admin.site.register(CustomUser)
+@admin.register(CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ['username', 'is_great_user', 'task_count', 'first_name', 'last_name', 'email', 'is_staff',
+                    'is_active', 'is_superuser', 'img_preview'];
+    ordering = ['first_name', 'last_name', 'email', 'is_staff', 'is_active', 'is_superuser', ]
+
+    def get_queryset(self, request):
+        return super().get_queryset(request).annotate(
+            task_count=Count('task__user')
+        )
