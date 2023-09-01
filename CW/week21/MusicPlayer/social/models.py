e684f9bdadf4bf12a5223b6ed1f7e57be25e884b
rezatn0934 <rezatn0934@gmail.com>
2023-09-01 09:53:06 +0330

    chore: define Playlist model

--- CW/week21/MusicPlayer/social/models.py
+++ CW/week21/MusicPlayer/social/models.py
@@ -1,3 +1,17 @@
 from django.db import models
-
+from django.db import models
+from user.models import User
+from songs.models import Song
 # Create your models here.
+
+
+class Playlist(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField(null=True, blank=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    songs = models.ManyToManyField(Song)
+
+    def __str__(self):
+        return self.title
+
+
