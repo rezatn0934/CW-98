ed8d2ce067bf7d0022ef696b6c2696b7a018beb7
rezatn0934 <rezatn0934@gmail.com>
2023-09-03 10:28:18 +0330

    chore: define UserLoginView

--- CW/week21/MusicPlayer/user/views.py
+++ CW/week21/MusicPlayer/user/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.contrib.auth import login
+from django.shortcuts import render, redirect, reverse
+from django.contrib.auth.views import LoginView
+from django.contrib import messages
+from django.urls import reverse_lazy
+from django.views import View
+
+from .forms import ArtistRegisterForm, ListenerRegisterForm, LoginForm
+
 
 # Create your views here.
+
+
+class UserLoginView(LoginView):
+    template_name = 'user/login.html'
+    redirect_authenticated_user = True
+    authentication_form = LoginForm
+    next_page = reverse_lazy('song:home')
+
+    def form_valid(self, form):
+        remember_me = form.cleaned_data.get('remember_me')
+
+        if not remember_me:
+            self.request.session.set_expiry(0)
+            self.request.session.modified = True
+        return super().form_valid(form)
