f2c03326e431c2acbaed4524a06861f2d4e79de4
rezatn0934 <rezatn0934@gmail.com>
2023-09-03 10:51:32 +0330

    chore: define admin site titles

--- CW/week21/MusicPlayer/user/admin.py
+++ CW/week21/MusicPlayer/user/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import AbstractUser, Band, Artist, Listener
+from .forms import ArtistRegisterForm
 
-# Register your models here.
+
+admin.site.site_header = 'Music Player Management'
+admin.site.site_title = 'Colorlib Management'
+admin.site.index_title = 'Welcome to admin panel'
+
+
