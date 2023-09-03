5da1da9cca05a2d1003dbe20487c51e3a3924000
rezatn0934 <rezatn0934@gmail.com>
2023-09-03 10:35:01 +0330

    refac: add username to create user and superuser

--- CW/week21/MusicPlayer/user/manger.py
+++ CW/week21/MusicPlayer/user/manger.py
@@ -2,17 +2,17 @@ from django.contrib.auth.models import BaseUserManager
 
 
 class UserManager(BaseUserManager):
-    def create_user(self, email, password):
-        if not email:
+    def create_user(self, email, username, password):
+        if not (email and username):
             raise ValueError("Users must have an Email")
 
-        user = self.model(email=email)
+        user = self.model(email=email, username=username)
         user.set_password(password)
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, email, password):
-        user = self.create_user(email, password)
+    def create_superuser(self, email, username, password):
+        user = self.create_user(email=email, username=username, password=password)
         user.is_staff = True
         user.is_superuser = True
         user.save(using=self._db)
