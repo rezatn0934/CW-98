b7218d775f7609a99d6a64c26714f90d89d30747
rezatn0934 <rezatn0934@gmail.com>
2023-09-03 10:37:28 +0330

    chore: define __str__ for Band

--- CW/week21/MusicPlayer/user/models.py
+++ CW/week21/MusicPlayer/user/models.py
@@ -29,7 +29,10 @@ class AbstractUser(AbstractBaseUser, PermissionsMixin):
 class Band(models.Model):
     name = models.CharField(max_length=50)
     start_at = models.DateTimeField(auto_now_add=True, editable=False)
-    end_at = models.DateTimeField(blank=True, null=True, editable=True)
+    end_at = models.DateTimeField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
 
 
 class Artist(AbstractUser):
