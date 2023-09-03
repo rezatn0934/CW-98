282260d400b77202a44e39fde531a8ff03df1e85
rezatn0934 <rezatn0934@gmail.com>
2023-09-03 10:32:10 +0330

    chore: define logout path

--- CW/week21/MusicPlayer/user/urls.py
+++ CW/week21/MusicPlayer/user/urls.py
@@ -5,5 +5,6 @@ from . import views
 app_name = 'user'
 urlpatterns = [
     path('login/', views.UserLoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(next_page='user:login'), name='logout'),
     path('register/', views.UserRegisterView.as_view(), name='register')
 ]
