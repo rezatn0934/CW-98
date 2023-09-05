fcbd88ae6346287d8e72b393c8d2eb434505ed1e
rezatn0934 <rezatn0934@gmail.com>
2023-09-05 10:49:27 +0330

    chore: define sound_display in Song model

--- CW/week21/MusicPlayer/songs/models.py
+++ CW/week21/MusicPlayer/songs/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.safestring import mark_safe
 from user.models import Band
 # Create your models here.
 
@@ -13,7 +14,14 @@ class Song(models.Model):
     cover_photo = models.ImageField(verbose_name='Cover photo', upload_to='images/song', null=True, blank=True)
     audio_file = models.FileField(verbose_name='Audio File', upload_to='files/song')
     genres = models.ManyToManyField(Genre)
-    band = models.ForeignKey(Band, on_delete=models.PROTECT)
+    band = models.ForeignKey(Band, on_delete=models.PROTECT, null=True, blank=True)
+
+    @property
+    def sound_display(self):
+        if self.audio_file:
+            return mark_safe(f'<audio controls name="media"><source src="{self.audio_file.url}" type="audio/mpeg"></audio>')
+        return ""
+
 
     def __str__(self):
         return self.title
