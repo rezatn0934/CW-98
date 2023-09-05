9e76e0c4dfcc285e2916022b68e8d4ed4fc301cc
rezatn0934 <rezatn0934@gmail.com>
2023-09-05 12:37:17 +0330

    chore: define LikeView in social app

--- CW/week21/MusicPlayer/social/views.py
+++ CW/week21/MusicPlayer/social/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.http import JsonResponse, HttpResponseRedirect, HttpResponse
+from django.shortcuts import redirect
+from django.urls import reverse_lazy, reverse
+from django.views import View
+from django.views.generic import CreateView
+
+from .forms import CommentCreation
+from .models import Like, Comment
+from songs.models import Song
+
 
 # Create your views here.
+
+
+class LikeView(View):
+
+    def post(self, request):
+        song_id = request.POST.get('like')
+        is_like = request.POST.get('like_input')
+        if is_like == 'like':
+            Like.objects.create(user_id=request.user.id, song_id=song_id)
+        elif is_like == 'unlike':
+            Like.objects.filter(user_id=request.user.id, song_id=song_id).delete()
+
+        like_count = Song.objects.get(id=song_id).likes()
+        return JsonResponse({'like_count': like_count}, status=200)
+
