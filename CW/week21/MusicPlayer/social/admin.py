9003d341cbb8ba0a06b6de9f210444da2ffb2460
rezatn0934 <rezatn0934@gmail.com>
2023-09-05 12:27:32 +0330

    chore: define PlaylistAdmin  in social app

--- CW/week21/MusicPlayer/social/admin.py
+++ CW/week21/MusicPlayer/social/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from django.template.defaultfilters import truncatewords
+
+from .models import Playlist, Like, Comment
 
 # Register your models here.
+
+
+@admin.register(Playlist)
+class PlaylistAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'truncated_description', 'owner_user', 'songs_list')
+    list_filter = ('title', 'owner', )
+    search_fields = ('title__istartwith', 'owner__name__istartwith', )
+    list_select_related = ('owner', )
+    list_per_page = 30
+
+    def owner_user(self, obj):
+        return obj.owner.username
+
+    def songs_list(self, obj):
+        return ",".join([song.title for song in obj.songs.all()])
+
+    def truncated_description(self, obj):
+        if obj.description:
+            return truncatewords(obj.description, 10)
