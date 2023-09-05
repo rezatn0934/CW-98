39ccc7c0b3197b6df21d2ab3492d6ef504e03192
rezatn0934 <rezatn0934@gmail.com>
2023-09-05 10:27:44 +0330

    chore: define AbstractUserAdmin in User admin

--- CW/week21/MusicPlayer/user/admin.py
+++ CW/week21/MusicPlayer/user/admin.py
@@ -1,10 +1,30 @@
 from django.contrib import admin
-from .models import AbstractUser, Band, Artist, Listener
-from .forms import ArtistRegisterForm
+from django.contrib.auth.admin import UserAdmin
 
+from .models import AbstractUser, Artist, Listener
+from .forms import ArtistRegisterForm, CustomUserCreationForm, CustomUserChangeForm
 
 admin.site.site_header = 'Music Player Management'
 admin.site.site_title = 'Colorlib Management'
 admin.site.index_title = 'Welcome to admin panel'
 
+
+@admin.register(AbstractUser)
+class AbstractUserAdmin(UserAdmin):
+    form = CustomUserChangeForm
+    add_form = CustomUserCreationForm
+
+    fieldsets = [
+        [None, {'fields': ['username', 'email', 'password1', 'password2']}],
+        ['Personal Info', {'fields': ['name', ]}],
+        ['Permissions', {'fields': ['is_active', 'is_staff', 'is_superuser']}],
+    ]
+    add_fieldsets = [
+        [None, {'fields': ['username', 'email', 'password1', 'password2']}],
+        ['Personal Info', {'fields': ['name', ]}],
+        ['Permissions', {'fields': ['is_active', 'is_staff', 'is_superuser']}],
+    ]
+    list_display = ['username', 'email', 'name', 'last_login', 'date_joined', 'is_active', 'is_staff', 'is_superuser']
+    search_fields = ['username', 'email', 'name']
+    ordering = ['username', ]
 
