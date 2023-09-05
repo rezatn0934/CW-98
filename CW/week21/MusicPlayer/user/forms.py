fb39c9c1ba482c23c9b495d783baac81fad2de54
rezatn0934 <rezatn0934@gmail.com>
2023-09-05 10:26:25 +0330

    chore: define CustomUserCreationForm in User forms

--- CW/week21/MusicPlayer/user/forms.py
+++ CW/week21/MusicPlayer/user/forms.py
@@ -7,3 +7,42 @@ class CustomUserChangeForm(UserChangeForm):
     class Meta:
         model = AbstractUser
         fields = '__all__'
+
+
+class CustomUserCreationForm(UserCreationForm):
+    password1 = forms.CharField(label="Password", widget=forms.PasswordInput(
+        attrs={'class': 'form-control', 'id': 'password1', 'type': 'password', 'name': 'password1',
+               'placeholder': 'Password'}))
+    password2 = forms.CharField(label="Confirm Password", widget=forms.PasswordInput(
+        attrs={'class': 'form-control', 'id': 'password2', 'type': 'password', 'name': 'password2',
+               'placeholder': 'Password'}))
+
+    class Meta:
+        model = AbstractUser
+        fields = ('username', 'email', 'name', 'password1', 'password2')
+        labels = {
+            'username': 'Username',
+            'email': 'Email address',
+            'name': 'Name',
+        }
+        help_texts = {
+            'username': None,
+            'email': None,
+            'password1': None,
+            'password2': None,
+        }
+        widgets = {
+            'username': forms.TextInput(
+                attrs={'class': 'form-control', 'id': 'exampleInputUser', 'aria-describedby': 'usernameHelp',
+                       'placeholder': "Username"}
+            ),
+            'name': forms.TextInput(
+                attrs={'class': 'form-control', 'id': 'exampleInputName', 'placeholder': "Enter a Name"}
+            ),
+            'email': forms.TextInput(
+                attrs={'class': 'form-control', 'id': 'exampleInputEmail1', 'aria-describedby': 'emailHelp',
+                       'placeholder': "Enter E-mail"}
+            ),
+        }
+
+
