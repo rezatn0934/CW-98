aa686342ff7135f3ccc5cc249c923b1be5908bc6
rezatn0934 <rezatn0934@gmail.com>
2023-09-05 17:53:57 +0330

    chore: define UpdatePlayListView urls

--- CW/week21/MusicPlayer/social/urls.py
+++ CW/week21/MusicPlayer/social/urls.py
@@ -5,4 +5,5 @@ app_name = 'social'
 urlpatterns = [
     path('like/', views.LikeView.as_view(), name='like'),
     path('create_comment/', views.CreatCommentView.as_view(), name='create_comment'),
+    path('update_playlist/', views.UpdatePlayListView.as_view(), name='update_playlist'),
 ]
