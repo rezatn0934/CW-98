84cef66813aa98ca0d929f3a103cb483eb2e7bc8
rezatn0934 <rezatn0934@gmail.com>
2023-09-05 10:40:31 +0330

    chore: define ListenerUserAdmin in User admin

--- CW/week21/MusicPlayer/user/admin.py
+++ CW/week21/MusicPlayer/user/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
-from .models import AbstractUser, Artist, Listener
-from .forms import ArtistRegisterForm, CustomUserCreationForm, CustomUserChangeForm
+from .models import AbstractUser, Artist, Listener, Band
+from .forms import ArtistRegisterForm, ListenerRegisterForm, CustomUserCreationForm, CustomUserChangeForm
 
 admin.site.site_header = 'Music Player Management'
 admin.site.site_title = 'Colorlib Management'
@@ -13,7 +13,6 @@ admin.site.index_title = 'Welcome to admin panel'
 class AbstractUserAdmin(UserAdmin):
     form = CustomUserChangeForm
     add_form = CustomUserCreationForm
-
 
     fieldsets = [
         [None, {'fields': ['username', 'email', 'password1', 'password2']}],
@@ -54,9 +53,32 @@ class ArtistUserAdmin(AbstractUserAdmin):
 
     def songs_list(self, obj):
         return ",".join([song.title for song in obj.songs.all()])
+    songs_list.short_description = 'Songs List'
+
 
     def band_name(self, obj):
         if obj.band:
             return obj.band.name
 
+    band_name.short_description = 'Band Name'
 
+
+@admin.register(Listener)
+class ListenerUserAdmin(AbstractUserAdmin):
+    form = ListenerRegisterForm
+    modl = Listener
+    list_display = ['name', 'user_status', 'vip_until', 'username', 'email', 'is_active', 'is_staff',
+                    'is_superuser', 'last_login', 'date_joined', 'img_preview']
+    search_fields = ['username__istartwith', 'email__istartswith', 'name__istartswith']
+    ordering = ['date_joined', ]
+
+    fieldsets = (
+        *AbstractUserAdmin.fieldsets,
+        ('Listener Info', {'fields': ('user_status', 'vip_until')}),
+    )
+    add_fieldsets = (
+        *AbstractUserAdmin.add_fieldsets,
+        ('Listener Info', {
+            'fields': ('user_status', 'vip_until'),
+        }),
+    )
