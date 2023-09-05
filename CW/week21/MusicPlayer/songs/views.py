229c900282aeeea447fa9af9cde83bdf17d869bb
rezatn0934 <rezatn0934@gmail.com>
2023-09-05 12:15:08 +0330

    chore: define Home View Song Views

--- CW/week21/MusicPlayer/songs/views.py
+++ CW/week21/MusicPlayer/songs/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+
+from django.core.paginator import Paginator
+from django.shortcuts import redirect, reverse, render
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView
+
+from .mixins import LikeMixin
+from .models import Song
+from user.models import Artist
+from social.forms import CommentCreation
+
 
 # Create your views here.
+
+
+class Home(ListView):
+    template_name = 'song/songs.html'
+    model = Song
+    context_object_name = 'songs'
+    object_list = None
+
